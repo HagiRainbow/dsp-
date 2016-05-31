6efc1db4179cf1e1f971250546031104ca24e2dd
Hagirainbow <h8a9g11i@gmail.com>
2016-05-31 10:46:28 +0900

    dsp2を一部追加

---
+++ dsp2-1.c
@@ -0,0 +1,317 @@
+﻿//＊＊＊H28年度・DSP2-1＊＊＊
+/*初期設定*/
+#define _USE_MATH_DEFINES/*M_PIとか有効化*/
+#define maxN 20000/*多めに確保*/
+#define maxS 128/*多めに確保*/
+/*ヘッダ類*/
+#include <stdio.h>/*言うまでもないやつ*/
+#include <stdlib.h>/*関数exitのために必要*/
+#include <math.h>/*M_PIと関数sqrt,sin,cos,atan2,log2のために必要*/
+#include <time.h> /*時間計測用*/
+/*returnだけの関数をdifineのマクロで実装*/
+#define Abs2R(a,b) (a)*(a)+(b)*(b)//絶対値^2
+#define AbsR(a,b) sqrt(Abs2R((a),(b)))//絶対値
+#define Freq(a,b) 20*log10(AbsR((a),(b)))//振幅スペクトル
+#define Pha(a,b) (180*atan2((b),(a)))/M_PI//位相スペクトル
+#define Max(a,b) ((a)>(b)?(a):(b))
+/*構造体定義*/
+typedef struct {
+	double re;/*実*/
+	double im;/*虚*/
+} rid;
+clock_t start,end;/*時間計測用*/
+/*グローバル変数等*/
+rid xn[maxN],xn2[maxN],rtr[maxN],Rxx[maxN];//xn:計算前xk:計算後rtr:回転子
+int mcnt=0;//掛け算数カウンタ
+
+/*構造体加算*/
+rid sumR(rid r1,rid r2){
+	rid res;
+	res.re=r1.re+r2.re;
+	res.im=r1.im+r2.im;
+	return res;
+}
+/*構造体減算*/
+rid difR(rid r1,rid r2){
+	rid res;
+	res.re=r1.re-r2.re;
+	res.im=r1.im-r2.im;
+	return res;
+}
+/*構造体乗算*/
+rid proR(rid r1,rid r2){
+	rid res;
+	res.re=r1.re*r2.re-r1.im*r2.im;
+	res.im=r1.im*r2.re+r1.re*r2.im;
+	return res;
+}
+/*共役複素数*/
+rid ccR(rid r1){
+	rid res;
+	res.re=r1.re;
+	res.im=-r1.im;
+	return res;
+}
+/*構造体除算*/
+rid quoR(rid r1,rid r2){
+	rid res;
+	double abs;
+	abs=AbsR(r2.re,r2.im);
+	printf("%lf\n",abs);
+	res.re=(r1.re*r2.re+r1.im*r2.im)/abs;
+	res.im=(r1.im*r2.re-r1.re*r2.im)/abs;
+	return res;
+}
+/*回転子計算(*rtr:回転子,N:数,fft:FFTorIFFT)*/
+void wid(rid *rtr,int N,int fft)
+{
+	int i,n;
+	n=N/2;
+	for(i=0;i<n;i++){
+		rtr[i].re=cos(((2*M_PI)/N)*i);
+		rtr[i].im=sin(((2*M_PI)/N)*i);
+		if(fft!=1){
+			rtr[i]=ccR(rtr[i]);//複素共役にする
+		}
+	}
+}
+//ファイル読み込み
+int read(rid d[],char fn[maxS])
+{
+	FILE *fp;
+	int i=0;
+	if((fp=fopen(fn,"r"))==NULL){
+		printf("[%s]を開けませんでした.\n",fn);
+		exit(1);
+	}
+	while(fscanf(fp,"%lf",&d[i].re)!=EOF){
+		i++;
+	}
+	fclose(fp);
+	return i;
+}
+
+void RIread(rid d[],char fn[maxS],int N,int dft)
+{
+	FILE *fp;
+	int i=0;
+	if((fp=fopen(fn,"r"))==NULL){
+		printf("[%s]を開けませんでした.\n",fn);
+		exit(1);
+	}
+	for(i=0;i<N;i++){
+		if(dft==1){
+			if(fscanf(fp,"%lf\n",&d[i].re)==EOF){
+				printf("データが不足しています．\n");
+				exit(1);
+			}
+		}else{
+			if(fscanf(fp,"%lf %lf\n",&d[i].re,&d[i].im)==EOF){
+				printf("データが不足しています．\n");
+				exit(1);
+			}
+		}
+	}
+	fclose(fp);
+}
+//ファイル書き出し
+void RIwrite(rid d[],char fn[maxS],int N,int dft,int rt)
+{
+	FILE *fp;
+	int i=0;
+	if((fp=fopen(fn,"w"))==NULL){
+		printf("[%s]を開けませんでした.\n",fn);
+		exit(1);
+	}
+	for(i=0;i<N;i++){
+		if(dft!=1){
+			fprintf(fp,"%lf\n",d[i].re/(N-rt));
+		}else{
+			if(rt==1){
+				fprintf(fp,"%lf\t%lf\n",d[i].re,d[i].im);
+			}else{
+				fprintf(fp,"%lf\t%lf\n",Freq(d[i].re,d[i].im),Pha(d[i].re,d[i].im));
+			}
+		}
+	}
+	fclose(fp);
+}
+//ファイル書き出し2
+void Rwrite(rid d[],char fn[maxS],int N,int m)
+{
+	FILE *fp;
+	int i=0;
+	if((fp=fopen(fn,"w"))==NULL){
+		printf("[%s]を開けませんでした.\n",fn);
+		exit(1);
+	}
+	for(i=0;i<m+1;i++){
+		fprintf(fp,"%lf\n",(d[i].re/N));
+	}
+	fclose(fp);
+}
+/*ビットリハーサル(*xn:計算前データ,N:数)*/
+int bitr(int *bit,int N)
+{
+	int n,i,j;
+	n=(int)(log10(N)/log10(2));//底の変換公式利用
+	//計算の本体
+	for(i=0;i<N;i++){
+		bit[i]=0;//初期化
+		for(j=0;j<n;j++){
+			bit[i]+=((i>>j)&1)<<(n-1-j);//bitシフト方式
+		}
+	}
+	return n;
+}
+/*ビットリハーサルによる入れ替え関数*/
+int rrid(rid *xn,int N)
+{
+	int bit[maxN],i,n=0;
+	rid buf[maxN];
+	n=bitr(bit,N);//ビットリハーサル
+	//計算前データを待避
+	for(i=0;i<N;i++){
+		buf[i]=xn[i];
+	}
+	//計算前データを並び替え
+	for(i=0;i<N;i++){
+		//printf("%d\n",bit[i]);
+		xn[bit[i]]=buf[i];
+	}
+	return n;
+}
+/*バタフライ
+a-----------+---
+       -   -    
+        - -     
+         -      
+        - -     
+       -   -    
+b-*rtr--*-1-+---
+このグループがj、グループ内での計算数がk
+*/
+/*FFTIFFT本体*/
+void FFTIFFT(rid *xn,rid *rtr,int N,int n)
+{
+	int i,j,k,bg=N/2,bc=1,ih,il,rn;
+	rid buf;//回転子かけた後に保管するもの
+	start=clock();
+	for(i=0;i<n;i++){
+		//バタフライ演算するだけ
+		for(j=0;j<bg;j++){
+			//蝶グループのmaxはどんどん減っていく
+			for(k=0;k<bc;k++){
+				//蝶内での計算順のmaxはどんどん増えていく(bg*bc=2/N)
+				ih=j*bc*2+k;//一段目は一つの蝶の中でk個ずれる。蝶ごとに2^i*2個ずつづれていく
+				il=ih+bc;//二段目は一段目より2^iだけ多いはず。
+				rn=k*bg;//回転子の番号。
+				buf=proR(xn[il],rtr[rn]);//二段目×回転子
+				xn[il]=difR(xn[ih],buf);//二段目
+				xn[ih]=sumR(xn[ih],buf);//一段目
+				mcnt++;
+			}
+		}
+		bg/=2;//蝶グループはどんどん/2になる。
+		bc*=2;//計算順はどんどん2倍になる。(=2^i)
+	}
+	end=clock();
+}
+//窓関数類
+void hamming(rid d[],int N)
+{
+	int i;
+	for(i=0;i<N;i++){
+		d[i].re*=(0.54-0.46*cos((2*M_PI*i)/N));
+	}
+}
+
+void hanning(rid d[],int N)
+{
+	int i;
+	for(i=0;i<N;i++){
+		d[i].re*=(0.50-0.50*cos((2*M_PI*i)/N));
+	}
+}
+
+void black(rid d[],int N)
+{
+	int i;
+	for(i=0;i<N;i++){
+		d[i].re*=(0.42-0.50*cos((2*M_PI*i)/N)+0.08*cos((4*M_PI*i)/N));
+	}
+}
+//構造体の定数倍
+void rid_mult(rid d[],double n,int N)
+{
+	int i;
+	for(i=0;i<N;i++){
+		d[i].re*=n;
+	}
+}
+
+int main()
+{
+	char fin[maxS],fin2[maxS],fout[maxS];
+	int rt=0,N,win,i,n=0,mode=1,in1N=0,in2N=0,M,m,n2;
+	//最初の質問
+	printf("H28年度・DSP2-1・番号25\n");
+	printf("説明:textデータからFFTもしくはIFFTを用いて自己、相互相関関数を求めます。\n");
+	printf("入力ファイル:2つのデータファイル(自己相関は同じ、相互相関は違うファイル)\n");
+	printf("出力ファイル:上記のいずれも1つのデータファイル\n\n");
+	//中身をすべて0にする
+	for(i=0;i<maxN;i++){
+		xn[i].re=0.0;
+		xn2[i].re=0.0;
+		xn[i].im=0.0;
+		xn2[i].im=0.0;
+	}
+	printf("入力ファイル名(1):");
+	scanf("%s",&fin);
+	in1N=read(xn,fin);
+	printf("入力ファイル名(2):");
+	scanf("%s",&fin);
+	in2N=read(xn2,fin);
+	printf("出力ファイル名:");
+	scanf("%s",&fout);
+	printf("処理中...\n");
+	N=Max(in1N,in2N);
+	//データ読み取り
+	printf("ファイル(1):%d個 ファイル(2):%d個 ∴N=%d\n",in1N,in2N,N);
+	printf("最大の時間差(0～%d)：",N-1);
+	scanf("%d",&m);
+	//点数を求める
+	M=1;
+	while(M<(N+m)){
+		M*=2;
+	}
+	printf("M=%d\n",M);
+	wid(rtr,M,1);//回転子計算
+	printf("回転子計算完了\n");
+	n=rrid(xn,M);//bitリハーサル,返り値はfftに渡す物。
+	n2=rrid(xn2,M);//bitリハーサル,返り値はfftに渡す物。
+	printf("ビットリハ完了\n");
+	//FFT本体関数呼び出し(この中にバタフライ演算も実装)
+	FFTIFFT(xn,rtr,M,n);
+	FFTIFFT(xn2,rtr,M,n2);
+	printf("FFT完了\n");
+	//1個目を複素共役にしたうえで2個目と乗算
+	for(i=0;i<M;i++){
+		Rxx[i]=proR(ccR(xn[i]),xn2[i]);
+	}
+	wid(rtr,M,0);//回転子計算
+	printf("回転子計算完了\n");
+	n=rrid(Rxx,M);//bitリハーサル,返り値はfftに渡す物。
+	printf("ビットリハ完了\n");
+	//FFT本体関数呼び出し(この中にバタフライ演算も実装)
+	FFTIFFT(Rxx,rtr,M,n);
+	printf("IFFT完了\n");
+	//データ書き出し
+	Rwrite(Rxx,fout,M*N,m);
+	printf("処理終了\n");
+	printf("掛け算数:%d 所要時間:%lf\n",mcnt,((double)(end-start)/CLOCKS_PER_SEC));
+	printf("結果は「%s」に出力されています．\n",fout);
+	getchar();
+	getchar();
+	return 0;
+}
---
+++ dsp2-2.c
@@ -0,0 +1,247 @@
+﻿//＊＊＊H28年度・DSP2-2＊＊＊
+/*初期設定*/
+#define _USE_MATH_DEFINES/*M_PIとか有効化*/
+#define maxN 200/*多めに確保*/
+#define maxS 128/*多めに確保*/
+#define Limit 0//これ以下の値は0にする
+//bmp書き込み用の設定
+#define Hsize 54
+#define Csize 3
+#define DIsize 40
+/*ヘッダ類*/
+#include <stdio.h>/*言うまでもないやつ*/
+#include <stdlib.h>/*関数exitのために必要*/
+#include <math.h>/*M_PIと関数sqrt,sin,cos,atan2,log2のために必要*/
+/*returnだけの関数をdifineのマクロで実装*/
+
+/////////////////////////////////////////////////////
+// 構造体 BITMAPFILEHEADER
+/////////////////////////////////////////////////////
+#pragma pack(1)
+typedef struct TagBITMAPFILEHEADER{//14byte
+	unsigned short bfType;			// ファイルタイプ("BM"：0x4D42が入る)
+	unsigned long  bfSize;			// ファイルサイズ
+	unsigned short bfReserved1;		// 予約領域（0）
+	unsigned short bfReserved2;		// 予約領域（0）
+	unsigned long  bfOffBits;		// ファイル先頭からみたデータの位置（byte）
+} BITMAPFILEHEADER;
+#pragma pack()
+/////////////////////////////////////////////////////
+// 構造体 BITMAPINFOHEADER
+/////////////////////////////////////////////////////
+typedef struct{//40byte
+	unsigned long biSize;			// BITMAPINFOHEADERのサイズ（40[byte]）
+	         long biWidth;			// 幅（ピクセル）
+	         long biHeight;			// 高さ（ピクセル）
+	unsigned short biPlanes;		// カラープレーン数（1）
+	unsigned short biBitCount;		// ビクセル当たりビット数（1,4,8,24,32bit）
+	unsigned long biCompression;		// 圧縮（0：なし）
+	unsigned long biSizeimage;		// 画像データサイズ（[byte]：0でよい）
+	         long biXPixPerMeter;		// 水平解像度：m当たり画素数（ピクセル：0でよい）
+	         long biYPixPerMeter;		// 垂直解像度：m当たり画素数（ピクセル：0でよい）
+	unsigned long biClrUsed;		// カラーパレット数:色数（0でよい）
+	unsigned long biClrImportant;		// 重要なカラーインデックス数（0でよい）
+} BITMAPINFOHEADER;
+/////////////////////////////////////////////////////
+// 構造体 カラーパレット　RGBQuad
+/////////////////////////////////////////////////////
+typedef struct{//1,4,8のときのみそれぞれ2,16,256組の配列を確保
+	unsigned char blue;			// Bule値
+	unsigned char green;			// Green値
+	unsigned char red;			// Red値
+	unsigned char re;			// Reserve値
+} RGBQUAD;
+/////////////////////////////////////////////////////
+// 構造体 データ保存　RGB
+/////////////////////////////////////////////////////
+typedef struct{
+	unsigned char blue;			// Bule値
+	unsigned char green;			// Green値
+	unsigned char red;			// Red値
+} RGB_DATA;
+
+BITMAPFILEHEADER bmp_file;
+BITMAPINFOHEADER bmp_info;
+RGB_DATA *rgb;
+
+double **xn,**xk;
+
+int bmprgb(char fnamei[maxS],char fnameo[maxS])
+{
+	int dbyte,dsize,i;
+	FILE *fin;
+	FILE *fout;
+	if((fin=fopen(fnamei,"rb"))==NULL){
+		exit(-1);
+	}
+	if((fout=fopen(fnameo,"w"))==NULL){
+		exit(-1);
+	}
+	//bmp_file
+	fseek(fin,0,SEEK_SET);	//ファイル読み込み位置をファイルの先頭バイトへ
+	fread(&bmp_file.bfType, sizeof(bmp_file.bfType),1,fin);//ヘッダ読み込み例
+	printf("BM:%d\n",bmp_file.bfType);
+	fread(&bmp_file.bfSize, sizeof(bmp_file.bfSize),1,fin);
+	printf("ファイルサイズ:%d[Byte]\n",bmp_file.bfSize);
+	fread(&bmp_file.bfReserved1, sizeof(bmp_file.bfReserved1),1,fin);//0
+	fread(&bmp_file.bfReserved2, sizeof(bmp_file.bfReserved2),1,fin);//0
+	fread(&bmp_file.bfOffBits, sizeof(bmp_file.bfOffBits),1,fin);
+	printf("データ開始位置:%d[Byte]\n",bmp_file.bfOffBits);
+	//bmp_info
+	fread(&bmp_info.biSize, sizeof(bmp_info.biSize),1,fin);//ヘッダ読み込み例
+	printf("info部サイズ:%d[Byte]\n",bmp_info.biSize);
+	fread(&bmp_info.biWidth, sizeof(bmp_info.biWidth),1,fin);
+	fread(&bmp_info.biHeight, sizeof(bmp_info.biHeight),1,fin);
+	printf("画素数:%d×%d\n",bmp_info.biWidth,bmp_info.biHeight);
+	fread(&bmp_info.biPlanes, sizeof(bmp_info.biPlanes),1,fin);//1
+	fread(&bmp_info.biBitCount, sizeof(bmp_info.biBitCount),1,fin);
+	dbyte=bmp_info.biBitCount/8;
+	printf("bit/ピクセル:%d[bit](=%d[byte])\n",bmp_info.biBitCount,dbyte);
+	fread(&bmp_info.biCompression, sizeof(bmp_info.biCompression),1,fin);//0以外は圧縮有?
+	printf("圧縮有無,フォーマット:%d\n",bmp_info.biCompression);
+	fread(&bmp_info.biSizeimage, sizeof(bmp_info.biSizeimage),1,fin);
+	printf("データ部サイズ?:%d[Byte]\n",bmp_info.biSizeimage);
+	fread(&bmp_info.biXPixPerMeter, sizeof(bmp_info.biXPixPerMeter),1,fin);
+	fread(&bmp_info.biYPixPerMeter, sizeof(bmp_info.biYPixPerMeter),1,fin);
+	printf("解像度:%d×%d\n",bmp_info.biXPixPerMeter,bmp_info.biYPixPerMeter);
+	fread(&bmp_info.biClrUsed, sizeof(bmp_info.biClrUsed),1,fin);
+	printf("カラーパレット数:%d\n",bmp_info.biClrUsed);
+	fread(&bmp_info.biClrImportant, sizeof(bmp_info.biClrImportant),1,fin);
+	printf("カラーインデックス数:%d\n",bmp_info.biClrImportant);
+	//ここからデータ
+	dsize=(bmp_file.bfSize-bmp_file.bfOffBits)/dbyte;
+	printf("画素数:%d\n",dsize);
+	rgb=(RGB_DATA*)malloc(dsize*sizeof(RGB_DATA));
+	for(i=0;i<dsize;i++){
+		fread(&rgb[i].blue, sizeof(rgb[i].blue),1,fin);
+		//fprintf(fout,"%d,",rgb[i].blue);
+		fread(&rgb[i].green, sizeof(rgb[i].green),1,fin);
+		//fprintf(fout,"%d,",rgb[i].green);
+		fread(&rgb[i].red, sizeof(rgb[i].red),1,fin);
+		//fprintf(fout,"%d,\n",rgb[i].red);
+	}
+	//printf("text書き出し完了\n");
+	fclose(fin);
+	fclose(fout);
+	return bmp_info.biWidth;
+}
+
+//ファイル書き出し2
+void R2write(double **d,char fn[maxS],int N)
+{
+	FILE *fp;
+	int i,j;
+	if((fp=fopen(fn,"w"))==NULL){
+		printf("[%s]を開けませんでした.\n",fn);
+		exit(1);
+	}
+	for(j=0;j<N;j++){
+		for(i=0;i<N;i++){
+			fprintf(fp,"%lf\t",d[i][j]);
+		}
+		fprintf(fp,"\n");
+	}
+	fclose(fp);
+}
+
+//行列を計算する
+void Mul_Matp(int N,double **d1,double **d2,double **res)
+{
+	int i,j,k;
+	for(j=0;j<N;j++){
+		for(i=0;i<N;i++){
+			res[i][j]=0.0;
+			for(k=0;k<N;k++){
+				res[i][j]+=d1[i][k]*d2[k][j];
+			}
+		}
+	}
+}
+
+void Trans_Matp(int N,double **d,double **res)
+{
+	int i,j;
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			res[i][j]=d[j][i];
+		}
+	}
+}
+
+double CnkN(double n,double k,double N)
+{
+	if(k==0){
+		return 1/sqrt(N);
+	}else{
+		return sqrt(2/N)*cos(((2*n+1)*k*M_PI)/(2*N));
+	}
+}
+
+//DCT処理
+void DCTIDCT2(int N,int mode)
+{
+	int k,n,i;
+	//double c[maxN][maxN],buf[maxN][maxN],ct[maxN][maxN];
+	double **c,**buf,**ct;
+	c=(double **)calloc(N,sizeof(double *));//doubleポインタ型×N個を確保
+	for(i=0;i<N;i++)c[i]=(double *)calloc(N,sizeof(double));//N個にdouble×Nのメモリ配列を確保
+	buf=(double **)calloc(N,sizeof(double *));//doubleポインタ型×N個を確保
+	for(i=0;i<N;i++)buf[i]=(double *)calloc(N,sizeof(double));//N個にdouble×Nのメモリ配列を確保
+	ct=(double **)calloc(N,sizeof(double *));//doubleポインタ型×N個を確保
+	for(i=0;i<N;i++)ct[i]=(double *)calloc(N,sizeof(double));//N個にdouble×Nのメモリ配列を確保
+	for(k=0;k<N;k++){
+		for(n=0;n<N;n++){
+			c[n][k]=CnkN(n,k,N);
+			//printf("%d,%d,%lf\n",n,k,c[n][k]);
+		}
+	}
+	Trans_Matp(N,c,ct);
+	if(mode==2){
+		Mul_Matp(N,c,xn,buf);
+		Mul_Matp(N,buf,ct,xk);
+	}else{
+		Mul_Matp(N,ct,xn,buf);
+		Mul_Matp(N,buf,c,xk);
+	}
+	for(i=0;i<N;i++)free(c[i]);free(c);
+	for(i=0;i<N;i++)free(buf[i]);free(buf);
+	for(i=0;i<N;i++)free(ct[i]);free(ct);
+}
+
+int main()
+{
+	int mode=0,N,i,j;
+	char fnamei[maxS],fnameo[maxS];
+	printf("2016年度・dsp2-2・5J25\n");
+	printf("読み取ったbmpをYCC->DCTします。\n");
+	printf("読み込むファイル名を入力(拡張子含む)-->");
+	scanf("%s",fnamei);
+	printf("出力ファイル名を入力(拡張子含む)-->");
+	scanf("%s",fnameo);
+	N=bmprgb(fnamei,fnameo);
+	printf("動的確保\n");
+	xn=(double **)calloc(N,sizeof(double *));//doubleポインタ型×N個を確保
+	for(i=0;i<N;i++)xn[i]=(double *)calloc(N,sizeof(double));//N個にdouble×Nのメモリ配列を確保
+	xk=(double **)calloc(N,sizeof(double *));//doubleポインタ型×N個を確保
+	for(i=0;i<N;i++)xk[i]=(double *)calloc(N,sizeof(double));//N個にdouble×Nのメモリ配列を確保
+	for(j=0;j<N;j++){
+		for(i=0;i<N;i++){
+			xn[i][j]=rgb[i+j*120].red*0.299+rgb[i+j*120].green*0.587+rgb[i+j*120].blue*0.114;
+		}
+	}
+	printf("YCC変換完了\n");
+	DCTIDCT2(N,1);
+	for(j=0;j<N;j++){
+		for(i=0;i<N;i++){
+			if(xk[i][j]<Limit){
+				xk[i][j]=0;
+			}
+		}
+	}
+	R2write(xk,fnameo,N);
+	for(i=0;i<N;i++)free(xn[i]);free(xn);
+	for(i=0;i<N;i++)free(xk[i]);free(xk);
+	getchar();
+	getchar();
+	return 0;
+}
